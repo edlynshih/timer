9965a5cd634c4e2738b4bfb95089be388d4a3394
Edlyn Shih <edlyn0418@gmail.com>
2022-08-06 13:29:04 -0400

    Implement an alarm clock which will beep after a specified amount of time. Takes in an unlimited number of alarms using command line arguments.

---
+++ timer1.js
@@ -0,0 +1,10 @@
+for (let i = 2; i < process.argv.length; i++) {
+let time = process.argv[i];
+  if(!isNaN(time) && time > 0) {
+    setTimeout(() => {
+      require("child_process").exec("afplay /System/Library/Sounds/Glass.aiff");
+    }, process.argv[i] * 1000);
+  }
+}
+
+
